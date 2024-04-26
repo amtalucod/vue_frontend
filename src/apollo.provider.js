3308cdcc15fc3fff3ea74da50cb1696a0715da41
amtalucod <alfredtalucod@yahoo.com>
2024-04-26 15:25:31 +0800

    added envi uri for development and deployment

--- src/apollo.provider.js
+++ src/apollo.provider.js
@@ -2,10 +2,13 @@ import { InMemoryCache, ApolloClient } from "@apollo/client";
 import { createApolloProvider } from "@vue/apollo-option";
 
 const cache = new InMemoryCache();
+const developmentUri = "http://localhost:3000/graphql";
+const productionUri = "https://rails-backend-xks1.onrender.com/graphql";
+const uri = process.env.NODE_ENV === "production" ? productionUri : developmentUri;
 
 const apolloClient = new ApolloClient({
     cache,
-    uri: "http://localhost:3000/graphql",
+    uri,
 });
 
 export const provider = createApolloProvider({
