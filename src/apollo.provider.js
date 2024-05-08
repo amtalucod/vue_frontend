92f3d5dc05363efcda821888a9ce25ca9208f120
amtalucod <alfredtalucod@yahoo.com>
2024-05-08 13:45:40 +0800

    change backend

--- src/apollo.provider.js
+++ src/apollo.provider.js
@@ -3,7 +3,7 @@ import { createApolloProvider } from "@vue/apollo-option";
 
 const cache = new InMemoryCache();
 const developmentUri = "http://localhost:3000/graphql";
-const productionUri = "https://rails-backend-xks1.onrender.com/graphql";
+const productionUri = "https://rails-backend-9sok.onrender.com/graphql";
 const uri = process.env.NODE_ENV === "production" ? productionUri : developmentUri;
 
 const apolloClient = new ApolloClient({
